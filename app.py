428d8fc516896a79fbea8f0c4da5b0f42138bf3e
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-25 23:38:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -39,32 +39,7 @@ def get_data(message):
     return dt, author, text
 
 @st.cache
-def read_file(file_name, date_format):    
-#     date_formats = {'dd.mm.yyyy': '%d.%m.%Y, %H:%M', 'dd.mm.yy': '%d.%m.%y, %H:%M'}
-#     result = []
-#     message_data = [] 
-#     dt, author = None, None 
-
-#     with open(file_name, 'r', encoding='utf=8') as file:
-#         messages = file.readlines()
-
-#     for line in messages:
-#         line = line.strip()
-# 		if 'Данное сообщение удалено' in line or '<Без медиафайлов>' in message:
-# 			messages.remove(message)
-			
-#         if starts_with_date(line): 
-#             if len(message_data) > 0: 
-#                 result.append([dt, author, ' '.join(message_data)]) 
-
-#         message_data.clear() 
-#         dt, author, message = get_data(line) 
-#         message_data.append(message) 
-
-#     else:
-#         message_data.append(line)
-        
-#     return result
+def read_file(file_name, date_format):   
 	date_formats = {'dd.mm.yyyy': '%d.%m.%Y, %H:%M', 'dd.mm.yy': '%d.%m.%y, %H:%M'}
 	result = []
 	message_data = [] 
@@ -74,7 +49,7 @@ def read_file(file_name, date_format):
 
 	for line in messages:
 		line = line.strip()
-		if 'Данное сообщение удалено' in line or '<Без медиафайлов>' in line:
+		if 'Данное сообщение удалено' in line or '<Без медиафайлов>' in line or line == '':
 			messages.remove(line)
 	
 		if starts_with_date(line): 
