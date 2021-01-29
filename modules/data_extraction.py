c88725e47ff373eb5dda13444074b3e6558fd922
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-29 15:20:17 +0600

    Update data_extraction.py

--- modules/data_extraction.py
+++ modules/data_extraction.py
@@ -1,61 +1,59 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st 
+import pandas as pd 
 import re
 
-def starts_with_dt(line):
-    pattern = '^([0-2][0-9]|(3)[0-1])(\.)(((0)[0-9])|((1)[0-2]))(\.)(\d{2}|\d{4})(,)? ([0-9])|([0-9]):([0-9][0-9])$'
-    result = re.match(pattern, line)
+from datetime import datetime
+
+def starts_with_dt(message):
+	pattern = '^([0-2][0-9]|(3)[0-1])(\.)(((0)[0-9])|((1)[0-2]))(\.)(\d{2}|\d{4})(,)? ([0-9])|([0-9]):([0-9][0-9])$'
+    result = re.match(pattern, message)
 
     if result:
         return True
 
     return False
 
-def get_data(line):
-    split_line = line.split(' - ') 
-    
-    dt = split_line[0]
-    date, time = dt.split(', ')  
 
-    message = ' '.join(split_line[1:]) 
-    
-    split_message = message.split(': ') 
-    author = split_message[0] 
-    text = ' '.join(split_message[1:]) 
-    
-    return date, time, author, text
+def get_data(message):
+	splitted = message.split(' - ')
+
+	dt = splitted[0]
+	date = dt.split(', ')[0]
+
+	author, text = splitted[1].split(': ')[0], splitted[1].split(': ')[1]
+
+	return date, author, text
 
 @st.cache
-def load_data(file_name, date_format):
-    with open(file_name, 'r', encoding='utf-8') as file:
-        lines = file.readlines()    
+def read_file(file_name):
+	with open(file_name, 'r', encoding='utf-8') as file:
+		messages = file.readlines()
 
-    return create_df(lines, date_format)
+	return messages
 
 @st.cache
-def create_df(data, date_format):
-    date_formats = {'dd.mm.yyyy': '%d.%m.%Y, %H:%M',
-                    'dd.mm.yy': '%d.%m.%y, %H:%M'}
+def create_df(messages, date_format):
+	date_formats = {'dd.mm.yyyy': '%d.%m.%Y',
+					'dd.mm.yy': '%d.%m.%y'}
 
-    data = []
-    messages = []
+	data = []
+	l_messages = []
 
-    date, time, author = None, None
+	author, text = None, None
 
-    for line in data:
-        line = line.strip() 
-        if starts_with_dt(line): 
-            if len(messages) > 0:
-                data.append([date, time, author, ' '.join(messages)]) 
+	for message in messages:
+		if starts_with_dt(message):
+			if len(l_messages) > 0:
+				data.append([date, author, ' '.join(l_messages)])
 
-            messages.clear() 
-            date, time, author, text = get_data(line) 
-            messages.append(text)
+		l_messages.clear()
+		date, author, text = get_data(message)
+		l_messages.append(text)
 
-        else:
-            messages.append(line) 
-    
-    df = pd.DataFrame(data, columns=['Date', 'Time', 'Author', 'Text'])
-    df["Date"] = pd.to_datetime(df["Date"], format=date_formats[date_format])    
-    
-    return df
+		else:
+			l_messages.append(text)
+
+	df = pd.DataFrame(data, columns=['Date', 'Author', 'Text'])	
+	df['Date'] = pd.to_datetime(df['Date'], format=date_formats[date_format])
+
+	return df
