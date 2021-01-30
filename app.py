a694d0011aa5138b6dfe85db0920f31dc22285f8
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-30 12:33:52 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import streamlit as st
 
+def main():
+	st.title('WhatsApp Chat Analysis')
+	st.markdown('Приложение создано для анализа русского WhatsApp чата')
+
+	st.sidebar.subheader('Как экспортировать чат?')	
+	st.sidebar.text('1) Зайдите в чат-группу')
+	st.sidebar.text('2) Настройки ⋮ -> Еще -> Экспорт чата')
+	st.sidebar.text('3) Выберите "Без Файлов"')
+
+	st.sidebar.subheader('Выберите формат даты:')
+	date_format = st.sidebar.selectbox(label='', options=('dd.mm.yyyy', 'dd.mm.yy'), key=0)
+
+	st.sidebar.subheader('Выберите форму облака слов:')
+	form = st.sidebar.selectbox(label='', options=('Сердце', 'Мозг', 'Штурмовик'), key=0)
+
+	uploaded_file = st.file_uploader(type='txt')
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
