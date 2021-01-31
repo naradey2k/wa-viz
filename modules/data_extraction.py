df9bf7040e5c2b79a8618d59780c9c92ebcb7895
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-31 15:40:58 +0600

    Update data_extraction.py

--- modules/data_extraction.py
+++ modules/data_extraction.py
@@ -25,8 +25,9 @@ def get_data(message):
 	return date, author, text
 
 @st.cache
-def read_file(file_name):
+def read_file(file_name):	
 	with open(file_name, 'r', encoding='utf-8') as file:
+		file = file.decode('utf-8')
 		messages = file.readlines()
 
 	return messages
