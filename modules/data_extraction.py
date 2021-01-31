21a0c93848aa1162d497b48e4e10c0e472127ee0
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-31 16:00:59 +0600

    Update data_extraction.py

--- modules/data_extraction.py
+++ modules/data_extraction.py
@@ -28,7 +28,7 @@ def get_data(message):
 def read_file(file_name):	
 	messages = []
 	
-	with open(file_name, 'rb', encoding='utf-8') as file:			
+	with open(file_name, 'r', encoding='utf-8') as file:			
 		content = file.readlines()
 		for message in content:
 			messages.append(message.decode('utf-8'))
