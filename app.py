e7a91061343f1a834b2ffe64622ce8be6f958332
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-31 16:01:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ def main():
 	if uploaded_file is not None:
 # 		try:
 # 		data = extraction.read_file(uploaded_file)
-# 		data = str(uploaded_file)
+		data = uploaded_file.read().decode('utf-8')
 	
-		messages = extraction.read_file(uploaded_file)
+		messages = extraction.read_file(data)
 	
 		df = extraction.create_df(messages, date_format)
 
