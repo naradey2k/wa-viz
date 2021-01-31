9a3fe27890898d60c57a38a23f9722dcf82f7cc7
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-01-31 17:25:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ def main():
 		
 		try:
 			file = uploaded_file.read()
-		
+			
+			file = str(file, 'utf-8')
+			
 			df = read_file(file, date_format)
 		
 			with st.beta_expander('Самые активные дни'):
@@ -51,24 +53,25 @@ def main():
 			with beta_expander('Облако слов'):
 				st.pyplot(analysis.create_wc(df, form))
 			
-		except OSError as exc:
-			if exc.errno == 36:
-				df = extraction.create_df(file, date_format)
+		except OSError as exc:			
+			file = str(file, 'utf-8')
+				
+			df = extraction.create_df(file, date_format)
 
-				with st.beta_expander('Самые активные дни'):
-					st.pyplot(analysis.most_active_df(df))
+			with st.beta_expander('Самые активные дни'):
+				st.pyplot(analysis.most_active_df(df))
 
-				with st.beta_expander(''):
-					st.pyplot(analysis.most_active_authors(df))
+			with st.beta_expander(''):
+				st.pyplot(analysis.most_active_authors(df))
 
-					authors = pd.DataFrame(df[df['Author'] != None].value_counts().sort_values(by='columns', ascending=False), columns=['Имя', 'Место'])
-					authors.index += 1
-					authors.index.name = 'Место'	
+				authors = pd.DataFrame(df[df['Author'] != None].value_counts().sort_values(by='columns', ascending=False), columns=['Имя', 'Место'])
+				authors.index += 1
+				authors.index.name = 'Место'	
 
-					st.table(authors)
+				st.table(authors)
 
-				with beta_expander('Облако слов'):
-					st.pyplot(analysis.create_wc(df, form))
+			with beta_expander('Облако слов'):
+				st.pyplot(analysis.create_wc(df, form))
 
 # 		except:
 # 			error = sys.exc_info()[0]
