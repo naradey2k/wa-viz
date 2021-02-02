ece7be6fee1ec07314ab9c53763bc8dd7b73d4e8
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-02-02 20:10:48 +0600

    Update data_analysis.py

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -38,12 +38,12 @@ def create_wc(messages, form):
 	forms = {'Heart': 'forms/heart.png',
 			 'Brain': 'forms/brain.png',
 			 'Stormtrooper': 'forms/stormtrooper.png'}
+	
+	words = get_words(messages)
 
-    words = get_words(messages)
-
-    fdict = collections.Counter(words)
+	fdict = collections.Counter(words)
     
-    return wordcloud.WordCloud(background_color="white", max_font_size=80, random_state=0, width=800, height=480,
+	return wordcloud.WordCloud(background_color="white", max_font_size=80, random_state=0, width=800, height=480,
                                mask=np.array(Image.open(forms[form])), color_func=color_func,
                                font_path="fonts/Oswald-Regular.ttf") \
         .generate_from_frequencies({key: value for key, value in fdict.items() if value >= 20})
@@ -51,32 +51,32 @@ def create_wc(messages, form):
 
 @st.cache
 def plot_line_dates(data):
-    dates = map(list, *zip(data))[0]
+	dates = map(list, *zip(data))[0]
 
-    fdist = collections.Counter(dates)
+	fdist = collections.Counter(dates)
 
-    plot = go.Figure(data=go.Scatter(x=fdist.keys(), y=fdist.values()))
+	plot = go.Figure(data=go.Scatter(x=fdist.keys(), y=fdist.values()))
 
-    return plot
+	return plot
 
 @st.cache
 def plot_dates(data):
-    dates = map(list, *zip(data))[0]
+	dates = map(list, *zip(data))[0]
 
-    df = pd.DataFrame(data=dates, columns=['Дата'])
+	df = pd.DataFrame(data=dates, columns=['Дата'])
 
-    plot = px.histogram(df, x='Дата')
-    plot.layout.yaxis.label = 'Кол-во сообщений'
+	plot = px.histogram(df, x='Дата')
+	plot.layout.yaxis.label = 'Кол-во сообщений'
 
-    return plot
+	return plot
 
 @st.cache
 def plot_authors(data):
-    authors = map(list, *zip(data))[1]
+	authors = map(list, *zip(data))[1]
 
-    df = pd.DataFrame(data=authors, columns=['Автор'])
+	df = pd.DataFrame(data=authors, columns=['Автор'])
 
-    plot = px.histogram(df, x='Автор')
-    plot.layout.yaxis.label = 'Кол-во сообщений'
+	plot = px.histogram(df, x='Автор')
+	plot.layout.yaxis.label = 'Кол-во сообщений'
 
-    return plot 
+	return plot 
