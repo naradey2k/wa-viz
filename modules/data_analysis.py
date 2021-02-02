f10b3df861d563333d52beb8ce2a50b110bcd629
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-02-02 21:03:05 +0600

    Update data_analysis.py

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -16,17 +16,17 @@ from nltk.corpus import stopwords
 
 
 @st.cache
-def tokenize(message):
-	return [token for token in word_tokenize(message, language='russian')]
+def tokenize(text):
+	return [token for token in word_tokenize(text, language='russian')]
 
 @st.cache
-def get_words(messages):	
+def get_words(texts):	
 	words = []
 	lemmatizer = MorphAnalyzer()
 	stopwords = stopwords.words('russian')
 
-	for message in messages:
-		for token in tokenize(message):
+	for text in texts:
+		for token in tokenize(text):
 			if token not in stopwords:
 				token = lemmatizer.normal_forms(token)[0]
 				words.append(token)
@@ -34,49 +34,43 @@ def get_words(messages):
 	return words
 
 @st.cache
-def create_wc(messages, form):
+def create_wc(texts, form):
 	forms = {'Heart': 'forms/heart.png',
 			 'Brain': 'forms/brain.png',
 			 'Stormtrooper': 'forms/stormtrooper.png'}
-	
-	words = get_words(messages)
 
-	fdict = collections.Counter(words)
+    words = get_words(texts)
+
+    fdict = collections.Counter(words)
     
-	return wordcloud.WordCloud(background_color="white", max_font_size=80, random_state=0, width=800, height=480,
+    return wordcloud.WordCloud(background_color="white", max_font_size=80, random_state=0, width=800, height=480,
                                mask=np.array(Image.open(forms[form])), color_func=color_func,
                                font_path="fonts/Oswald-Regular.ttf") \
         .generate_from_frequencies({key: value for key, value in fdict.items() if value >= 20})
 
 
 @st.cache
-def plot_line_dates(data):
-	dates = map(list, *zip(data))[0]
+def plot_line_dates(dates):
+    fdist = collections.Counter(dates)
 
-	fdist = collections.Counter(dates)
+    plot = go.Figure(data=go.Scatter(x=fdist.keys(), y=fdist.values()))
 
-	plot = go.Figure(data=go.Scatter(x=fdist.keys(), y=fdist.values()))
-
-	return plot
+    return plot
 
 @st.cache
-def plot_dates(data):
-	dates = map(list, *zip(data))[0]
+def plot_dates(dates):
+    df = pd.DataFrame(data=dates, columns=['Дата'])
 
-	df = pd.DataFrame(data=dates, columns=['Дата'])
+    plot = px.histogram(df, x='Дата')
+    plot.layout.yaxis.label = 'Кол-во сообщений'
 
-	plot = px.histogram(df, x='Дата')
-	plot.layout.yaxis.label = 'Кол-во сообщений'
-
-	return plot
+    return plot
 
 @st.cache
-def plot_authors(data):
-	authors = map(list, *zip(data))[1]
+def plot_authors(authors):
+    df = pd.DataFrame(data=authors, columns=['Автор'])
 
-	df = pd.DataFrame(data=authors, columns=['Автор'])
+    plot = px.histogram(df, x='Автор')
+    plot.layout.yaxis.label = 'Кол-во сообщений'
 
-	plot = px.histogram(df, x='Автор')
-	plot.layout.yaxis.label = 'Кол-во сообщений'
-
-	return plot 
+    return plot 
