a5bded07c4fac06de8b52c15c9f8abf50e08454b
doksterium <56970175+naradey2k@users.noreply.github.com>
2021-02-03 15:58:16 +0600

    Update data_analysis.py

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -50,29 +50,29 @@ def create_wc(texts, form):
 
 
 @st.cache
-def plot_line_dates(dates):
+def plot_line_dates(dates, **kwargs):
 	fdist = collections.Counter(dates)
 	
 	df = pd.DataFrame(data=fdist.values(), columns=['Дата'], index=fdist.keys())
 	
-	plot = px.line(df, labels={'x':'Дата', 'y':'Кол-во сообщений'})
+	plot = px.line(df, labels={'x':'Дата', 'y':'Кол-во сообщений'}, **kwargs)
 
 	return plot
 
 @st.cache
-def plot_dates(dates):
+def plot_dates(dates, **kwargs):
 	fdist = collections.Counter(dates)	
 	
 	df = pd.DataFrame(data=fdist.values(), columns=['Дата'], index=fdist.keys()).value_counts().head(10)
 
-	plot = px.histogram(df, labels={'x':'Дата', 'y':'Кол-во сообщений'})
+	plot = px.histogram(df, labels={'x':'Дата', 'y':'Кол-во сообщений'}, **kwargs)
 
 	return plot
 
 @st.cache
-def plot_authors(authors):
+def plot_authors(authors, **kwargs):
 	df = pd.DataFrame(data=authors, columns=['Автор'])
 
-	plot = px.histogram(df, labels={'x':'Автор', 'y':'Кол-во сообщений'})	
+	plot = px.histogram(df, labels={'x':'Автор', 'y':'Кол-во сообщений'}, **kwargs)	
 
 	return plot 
